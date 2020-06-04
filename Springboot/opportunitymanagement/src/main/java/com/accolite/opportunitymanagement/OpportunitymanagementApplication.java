0c6ddc4a607f637c5d727984f28335c064a7ce88
= <=>
2020-06-04 10:28:22 +0530

    Spring boot naming conventions

---
+++ Springboot/opportunitymanagement/src/main/java/com/accolite/opportunitymanagement/OpportunitymanagementApplication.java
@@ -0,0 +1,13 @@
+package com.accolite.opportunitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportunitymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpportunitymanagementApplication.class, args);
+	}
+
+}
